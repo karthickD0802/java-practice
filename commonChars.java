6ad07e921d225e5092fe5cb13c683c57e62be738
karthickD0802 <karthickdharumar@gmail.com>
2026-07-23 08:45:13 +0530

    demo push

---
+++ commonChars.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+
+        int[] common = new int[26];
+        Arrays.fill(common, Integer.MAX_VALUE);
+
+        for (String word : words) {
+
+            int[] freq = new int[26];
+
+            for (char ch : word.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                common[i] = Math.min(common[i], freq[i]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            while (common[i] > 0) {
+                result.add(String.valueOf((char)(i + 'a')));
+                common[i]--;
+            }
+        }
+
+        return result;
+    }
+}
